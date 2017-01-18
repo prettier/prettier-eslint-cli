04a25cf627653adbd58ecd407b468f473484b362
Kent C. Dodds <kent@doddsfamily.us>
2017-01-18 07:58:30 -0700

    WIP: remove unused code

--- cli-test/tests/index.js
+++ cli-test/tests/index.js
@@ -2,8 +2,6 @@ import path from 'path'
 import fs from 'fs'
 import spawn from 'spawn-command'
 import pify from 'pify'
-// import glob from 'glob'
-// import yargsParser from 'yargs-parser'
 
 const pWriteFile = pify(fs.writeFile)
 const pReadFile = pify(fs.readFile)
@@ -49,56 +47,6 @@ test('prettier-eslint cli-test/fixtures/*1.js cli-test/fixtures/*2.js', async ()
   ])
 })
 
-// testCLIOutput('generate', 'generate')
-// testCLIOutput('generate --templates-dir guides --exercises-dir app --exercises-final-dir app-finished', 'generate-with-args')
-// testCLIOutput('generate --no-clean=true --exercises-dir src --exercises-final-dir src-final', 'generate-no-clean')
-// testCLIOutput('generate --ignore "**/*.ignore-me.js"', 'generate-ignore-one')
-// testCLIOutput('generate --ignore "**/*.ignore-me.js" "**/*.no-copy.js"', 'generate-ignore-multiple')
-// testCLIOutput('generate --silent-success', 'generate-silent-success')
-// testCLIOutput('generate --silent-all', 'generate-silent-all')
-
-// function testCLIOutput(args, fixture) {
-//   test(`prettier-eslint ${args}`, () => {
-//     return runCLIAndAssertFileOutput(args, path.resolve(__dirname, `../../test/fixtures/${fixture}`))
-//   })
-// }
-
-// async function runCLIAndAssertFileOutput(args, cwd) {
-//   const {exercisesDir = './exercises', exercisesFinalDir = './exercises-final'} = yargsParser(args)
-//   const stdout = await runPrettierESLintCLI(args, cwd).catch(getErrorLogger('runPrettierESLintCLI'))
-//   const snapshotTitleBase = `${args} in ${relativeizePath(cwd)}`
-//   expect(relativeizePath(stdout)).toMatchSnapshot(`${snapshotTitleBase} stdout`)
-//   const tree = dirTree(cwd)
-//   relativeizePathInTree(tree)
-//   expect(tree).toMatchSnapshot(`${snapshotTitleBase} file tree`)
-//   // cannot use Promise.all here because we need to make sure the snapshots are
-//   // taken in the correct order
-//   await expectDirectoryToMatchSnapshot(path.resolve(cwd, exercisesDir), `${snapshotTitleBase} exercises-dir`)
-//   await expectDirectoryToMatchSnapshot(path.resolve(cwd, exercisesFinalDir), `${snapshotTitleBase} exercises-final-dir`)
-// }
-
-// function expectDirectoryToMatchSnapshot(directory, snapshotTitle) {
-//   return pify(glob)(path.resolve(directory, '**/*'), {nodir: true})
-//     .then(readAllFilesAsPromise)
-//     .then(expectFilesToMatchSnapshot)
-//     .catch(getErrorLogger(`expectDirectoryToMatchSnapshot(${directory})`))
-//
-//   function readAllFilesAsPromise(files) {
-//     const allPromises = files.map(readFileAsPromise)
-//     return Promise.all(allPromises)
-//   }
-//
-//   function readFileAsPromise(file) {
-//     return pify(fs.readFile)(file, 'utf8')
-//       .then(contents => ({file: relativeizePath(file), contents}))
-//       .catch(getErrorLogger(`readFileAsPromise(${file})`))
-//   }
-//
-//   function expectFilesToMatchSnapshot(files) {
-//     expect(files).toMatchSnapshot(snapshotTitle)
-//   }
-// }
-
 function runPrettierESLintCLI(args = '', cwd = process.cwd()) {
   const isRelative = cwd[0] !== '/'
   if (isRelative) {
@@ -132,14 +80,3 @@ function runPrettierESLintCLI(args = '', cwd = process.cwd()) {
     })
   })
 }
-
-// function relativeizePath(stringWithAbsolutePaths) {
-//   return stringWithAbsolutePaths.replace(new RegExp(path.resolve(__dirname, '../../'), 'g'), '<projectRootDir>')
-// }
-
-// function relativeizePathInTree(tree) {
-//   tree.path = relativeizePath(tree.path)
-//   if (tree.children) {
-//     tree.children.forEach(relativeizePathInTree)
-//   }
-// }
