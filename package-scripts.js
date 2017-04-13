2285f69512aea3885b3881c171b1254ee050a294
Kent C. Dodds <kent@doddsfamily.us>
2017-04-13 11:53:58 -0600

    feat: upgrade to the latest prettier-eslint

--- package-scripts.js
+++ package-scripts.js
@@ -1,31 +1,39 @@
-/* eslint max-len:[2, 120] */
+const npsUtils = require('nps-utils')
+
+const series = npsUtils.series
+const concurrent = npsUtils.concurrent
+const rimraf = npsUtils.rimraf
+
 module.exports = {
   scripts: {
+    contributors: {
+      add: {
+        description: 'When new people contribute to the project, run this',
+        script: 'all-contributors add',
+      },
+      generate: {
+        description: 'Update the badge and contributors table',
+        script: 'all-contributors generate',
+      },
+    },
     commit: {
       description: 'This uses commitizen to help us generate well formatted commit messages',
       script: 'git-cz',
     },
     test: {
-      default: `nps --parallel test.unit,test.cli`,
-      unit: {
-        script: 'jest --coverage',
-        description: 'Run the unit tests',
-        watch: 'jest --watch',
-      },
-      cli: {
-        script: 'jest --config=cli-test/jest.config.json',
-        description: 'The E2E tests for the full CLI',
-        watch: 'nps test.cli --watch',
-      },
+      default: 'jest --coverage',
+      watch: 'jest --watch',
     },
     build: {
       description: 'delete the dist directory and run babel to build the files',
-      script: 'rimraf dist && babel --copy-files --out-dir dist --ignore *.test.js,__mocks__ src',
+      script: series(
+        rimraf('dist'),
+        'babel --copy-files --out-dir dist --ignore *.test.js,__mocks__ src'
+      ),
     },
-    lint: {description: 'lint the entire project', script: 'eslint .'},
-    format: {
-      description: 'Uses dist/index.js to format the project (formatting itself). Recommended to run the build first!',
-      script: './dist/index.js src/**/*.js cli-test/tests/index.js __mocks__/*.js package-scripts.js --write',
+    lint: {
+      description: 'lint the entire project',
+      script: 'eslint .',
     },
     reportCoverage: {
       description: 'Report coverage stats to codecov. This should be run after the `test` script',
@@ -33,20 +41,36 @@ module.exports = {
     },
     release: {
       description: 'We automate releases with semantic-release. This should only be run on travis',
-      script: 'semantic-release pre && npm publish && semantic-release post',
+      script: series(
+        'semantic-release pre',
+        'npm publish',
+        'semantic-release post'
+      ),
     },
     validate: {
       description: 'This runs several scripts to make sure things look good before committing or on clean install',
-      script: 'nps build && nps format && nps --parallel lint,test',
+      script: concurrent.nps('lint', 'build', 'test'),
     },
-    addContributor: {
-      description: 'When new people contribute to the project, run this',
-      script: 'all-contributors add',
-    },
-    generateContributors: {
-      description: 'Update the badge and contributors table',
-      script: 'all-contributors generate',
+    format: {
+      description: 'Formats everything with prettier-eslint',
+      script: 'prettier-eslint src/*.js src/**/*.js --write',
     },
   },
-  options: {silent: false},
+  options: {
+    silent: false,
+  },
 }
+
+// this is not transpiled
+/*
+  eslint
+  max-len: 0,
+  comma-dangle: [
+    2,
+    {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      functions: 'never'
+    }
+  ]
+ */
