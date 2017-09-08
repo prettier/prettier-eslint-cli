795a2953cb6d6ad28f721b856c369abf3c24e0d9
Kent C. Dodds <kent+github@doddsfamily.us>
2017-09-07 22:23:11 -0600

    fix: upgrade to prettier@8 (#103)

--- __mocks__/prettier-eslint.js
+++ __mocks__/prettier-eslint.js
@@ -5,7 +5,7 @@
 // so we just return some spies here and assert
 // that we're calling prettier-eslint APIs correctly
 // eslint-disable-next-line require-await
-const format = jest.fn(async ({text, filePath = ''}) => {
+const format = jest.fn(({text, filePath = ''}) => {
   if (text === 'MOCK_SYNTAX_ERROR' || filePath.includes('syntax-error')) {
     throw new Error('Mock error for a syntax error')
   } else if (filePath.includes('eslint-config-error')) {
