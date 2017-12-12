114de3128e6a2904b8807ff22fc4b052202d3ebb
Simon Fridlund <simon@fridlund.email>
2017-12-12 20:13:36 +0100

    fix: Remove all kebab-case options from argv
    Becuase `prettier-eslint` now handles prettierOptions differently we need to remove all kebab-case options as prettier doesn't support them. This is fine becuase all kebab-case options are added as cameCase options too by yargs.
    
    Fixes #121.

---
+++ src/argv.js
@@ -0,0 +1,11 @@
+function normalize(options) {
+  return Object.entries(options).reduce(
+    (optionsAccumulator, [key, value]) =>
+      key.includes("-")
+        ? optionsAccumulator
+        : { ...optionsAccumulator, [key]: value },
+    {}
+  );
+}
+
+export default normalize;
--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import "./add-exception-handler"; // want to do this first
 import getLogger from "loglevel-colored-level-prefix";
 import parser from "./parser";
 import formatFiles from "./format-files";
+import normalize from "./argv";
 
 const logger = getLogger({ prefix: "prettier-eslint-cli" });
 const args = process.argv.slice(2);
 
 logger.trace("Parsing args: ", args);
-const argv = parser.parse(args);
+let argv = parser.parse(args);
+
+argv = normalize(argv);
 
 formatFiles(argv);
